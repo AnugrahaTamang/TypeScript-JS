12d3e88b82331604b1d7de13ed2bf5b04702328d
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-07 22:02:12 +0545

    annotations

---
+++ Basic Concept/Annotations/note.ts
@@ -0,0 +1,17 @@
+//Annotations: Annotations are used to specify the data type of a variable, parameter, function return value and other types of values. Annotations help developers catch errors early in development by allowing them to specify what types of values can be assigned to a given variable or passed as an argument to a function.
+ 
+// Annotations for string
+let myName: string = "Anugraha Gomja";
+console.log(myName);
+
+//Number Annotations
+let num: number = 12;
+console.log(num)
+
+//Boolean annotation
+let  isvalue: boolean = true;
+isvalue = false;
+console.log(isvalue);
+
+//function Annotation
+
