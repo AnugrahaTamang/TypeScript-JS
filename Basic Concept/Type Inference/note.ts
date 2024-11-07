cb9c32f7139999b9e5207843bf99ad1917d55d74
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-07 22:05:40 +0545

    type inference

---
+++ Basic Concept/Type Inference/note.ts
@@ -0,0 +1,6 @@
+// Type Inference: Type Inference is a feature in typescript that allows the compiler to automatically determine the type of a variable based on its value.
+let tech = "TypeScript";
+let boolvalue = true;
+let numvalue = 23;
+console.log(tech, boolvalue, numvalue);
+console.log(typeof tech)
